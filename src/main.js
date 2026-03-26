cbde961fde281e23c23924675866139152b21bd9
Tanya Tychynska <tatyanazyeva@gmail.com>
2026-03-26 11:22:03 +0100

    HW-12-3

--- src/main.js
+++ src/main.js
@@ -14,10 +14,9 @@ form.addEventListener('submit', async (event) => {
     event.preventDefault();
     page = 1;
     clearGallery();
-    document.querySelector('.end-message').classList.add('hidden');
     hideLoadMoreButton();
     showLoader();
-    userInput = inputField.value;
+    userInput = inputField.value.trim();
     if (userInput === '') {
         iziToast.show({
             title: 'Empty form',
@@ -30,9 +29,8 @@ form.addEventListener('submit', async (event) => {
     try {
         const data = await getImagesByQuery(userInput, page);
         totalPages = Math.ceil(data.totalHits / perPage);
-    if (data.total === 0) {
+    if (data.hits.length === 0) {
         iziToast.show({
-            title: 'Try again',
             message: 'Sorry, there are no images matching your search query. Please try again!',
             color: 'blue',
         });
@@ -40,16 +38,16 @@ form.addEventListener('submit', async (event) => {
         return;
     }
 
-    createGallery(data.hits);
+        createGallery(data.hits);
+        checkBtnStatus();
     } catch (error) {
         iziToast.show({
             title: 'Error',
-            message: 'Something went wrong in createGallery',
+            message: `Error received: ${error}`,
             color: 'red',
         });
     } finally {
         hideLoader();
-        checkBtnStatus();
 
     }
     
@@ -63,7 +61,7 @@ loadMore.addEventListener('click', async e => {
     page += 1;
       try {
     const data = await getImagesByQuery(userInput, page);
-    if (data.total === 0) {
+    if (data.hits.length === 0) {
         iziToast.show({
             message: 'Sorry, there are no images matching your search query. Please try again!',
             color: 'blue',
@@ -72,6 +70,8 @@ loadMore.addEventListener('click', async e => {
         return;
     }
           createGallery(data.hits);
+          checkBtnStatus();
+
           
 // ! ====== Scroll
     const card = document.querySelector('.gallery-item');
@@ -84,15 +84,16 @@ loadMore.addEventListener('click', async e => {
     }
 // ! ====== end Scroll
 
-    } catch (error) {
-        iziToast.show({
+      } catch (error) {
+          page -= 1;
+          hideLoadMoreButton();
+          iziToast.show({
             title: 'Error',
             message: `Error received: ${error}`,
             color: 'red',
         });
     } finally {
         hideLoader();
-        checkBtnStatus();
 
     }
 })
